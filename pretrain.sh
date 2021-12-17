1443c2aba712dab169e6de6d98da13d16283cae2
yisenlin <ysl.130@qq.com>
2021-12-18 00:28:04 +0800

    fix arg not valid

--- pretrain.sh
+++ pretrain.sh
@@ -1,7 +1,9 @@
-OUTPUT_DIR="output/pretrain_mae_base_patch16_224"
+#!/bin/sh
+OUTPUT_DIR='output/pretrain_mae_base_patch16_224'
 DATA_PATH='/home/featurize/data'
 
 # batch_size can be adjusted according to the graphics card
+#rtx3060 almost 3min per epoch
 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=1 entry.py \
         --data_path ${DATA_PATH} \
         --mask_ratio 0.75 \
@@ -10,7 +12,6 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=1 entry.py
         --opt adamw \
         --opt_betas 0.9 0.95 \
         --warmup_epochs 40 \
-        #rtx3060 almost 3min per epoch
         --epochs 160 \
         --num_workers 5 \
         --output_dir ${OUTPUT_DIR}
