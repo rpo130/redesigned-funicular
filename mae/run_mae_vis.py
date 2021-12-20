603d925eb86ef62e8b0008efda88e9b90b32c912
yisenlin <ysl.130@qq.com>
2021-12-20 20:17:45 +0800

    debug mae

--- mae/run_mae_vis.py
+++ mae/run_mae_vis.py
@@ -26,6 +26,7 @@ from pathlib import Path
 
 from timm.models import create_model
 from torchvision import transforms
+from mae.masking_generator import imshow
 
 import mae.utils as utils
 import mae.modeling_pretrain as modeling_pretrain
@@ -109,16 +110,73 @@ def main(args):
         DATA_STD = (0.5,0.5,0.5)
 
         #save original img
+        #---------------------------
         mean = torch.as_tensor(DATA_MEAN).to(device)[None, :, None, None]
         std = torch.as_tensor(DATA_STD).to(device)[None, :, None, None]
         ori_img = img * std + mean  # in [0, 1]
+        print(f'unnorm : {img.mean()}, {img.var()}')
         img = ToPILImage()(ori_img[0, :])
         img.save(f"{args.save_path}/ori_img.jpg")
+        #---------------------------
+
+        #记录原始模型输入
+        #逆归一化之后
+        #-------------------------------------
+        old_input_img = ori_img;
+        # print(f'norm : {old_input_img.mean()}, {old_input_img.var()}')
+
+        old_input_img_squeeze = rearrange(old_input_img, 'b c (h p1) (w p2) -> b (h w) (p1 p2) c', p1=patch_size[0], p2=patch_size[0])
+        #normal
+        old_input_img_squeeze_normal = (old_input_img_squeeze - old_input_img_squeeze.mean(dim=-2, keepdim=True)) / (old_input_img_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6)
+
+        # print(f'norm aft conv1 data , mean-2 : {old_input_img.mean()}, diy : {old_input_img.sum()}, {old_input_img.shape}')
+        old_input_img_squeeze_fill = old_input_img_squeeze.clone();
+
+        #cal squeeze by considering before block
+        #-------------------------------------------------
+        lastNotBlockArea = None;
+        hit = 0;
+        for bi in range(len(old_input_img_squeeze_fill)) :
+            mask_batch = bool_masked_pos[bi];
+            for blocki in range(len(old_input_img_squeeze_fill[bi])) :
+                currBlock = old_input_img_squeeze_fill[bi][blocki];
+                mask_flag = mask_batch[blocki];
+                if mask_flag :
+                    if lastNotBlockArea != None :
+                        old_input_img_squeeze_fill[bi][blocki] = lastNotBlockArea;
+                        pass
+                else :
+                    pass;
+
+                hit += 1;
+                if lastNotBlockArea is None :
+                    lastNotBlockArea = currBlock;
+                else :
+                    old_w = int(hit * 0.3);
+                    new_w = hit - old_w
+                    lastNotBlockArea = ( old_w * lastNotBlockArea + new_w * currBlock ) / hit;
+            lastNotBlockArea = None;
+            hit = 0;
+        #-------------------------------------------------
+
+        #aft normal
+        old_input_img_squeeze_normal_squeeze = rearrange(old_input_img_squeeze_normal, 'b n p c -> b n (p c)')
+
+        old_input_img_squeeze_normal_squeeze[bool_masked_pos] = outputs
+
+        old_input_rec_img = rearrange(old_input_img_squeeze_normal_squeeze, 'b n (p c) -> b n p c', c=3)
+        old_input_rec_img = old_input_rec_img  * (old_input_img_squeeze_fill.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6) + old_input_img_squeeze_fill.mean(dim=-2, keepdim=True)
+
+        old_input_rec_img = rearrange(old_input_rec_img, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=14, w=14)
+
+        imshow(old_input_rec_img[0].clip(0,1))
+        #-------------------------------------
 
         img_squeeze = rearrange(ori_img, 'b c (h p1) (w p2) -> b (h w) (p1 p2) c', p1=patch_size[0], p2=patch_size[0])
-        img_norm = (img_squeeze - img_squeeze.mean(dim=-2, keepdim=True)) / (img_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6)
+        # img_norm = (img_squeeze - img_squeeze.mean(dim=-2, keepdim=True)) / (img_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6)
+        # img_norm = (old_input_img_squeeze_fill - old_input_img_squeeze_fill.mean(dim=-2, keepdim=True)) / (old_input_img_squeeze_fill.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6)
+        img_norm = img_squeeze
         img_patch = rearrange(img_norm, 'b n p c -> b n (p c)')
-
 
         #TODO need to construct using the same distribution but block are can't infer
         img_patch[bool_masked_pos] = outputs
@@ -129,10 +187,18 @@ def main(args):
         mask = rearrange(mask, 'b n (p c) -> b n p c', c=3)
         mask = rearrange(mask, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=14, w=14)
 
+        anti_mask = torch.zeros_like(img_patch)
+        anti_mask[bool_masked_pos] = 1
+        anti_mask = rearrange(anti_mask, 'b n (p c) -> b n p c', c=3)
+        anti_mask = rearrange(anti_mask, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=14, w=14)
+
         #save reconstruction img
         rec_img = rearrange(img_patch, 'b n (p c) -> b n p c', c=3)
-        rec_img = rec_img * (img_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6) + img_squeeze.mean(dim=-2, keepdim=True)
+        # rec_img = rec_img * (img_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6) + img_squeeze.mean(dim=-2, keepdim=True)
+        # rec_img = rec_img * (old_input_img_squeeze_fill.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6) + old_input_img_squeeze_fill.mean(dim=-2, keepdim=True)
         rec_img = rearrange(rec_img, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=14, w=14)
+        #only change predict area
+        rec_img[anti_mask.to(torch.bool)] = old_input_rec_img[anti_mask.to(torch.bool)]
         img = ToPILImage()(rec_img[0, :].clip(0,0.996))
         img.save(f"{args.save_path}/rec_img.jpg")
 
--- run_rebuild_pic.py
+++ run_rebuild_pic.py
@@ -6,8 +6,9 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 pre_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), "../.."))
 
 if __name__=='__main__':
-    img_path = r'./mae/files//0_29.jpg'
-    # img_path =  "./mae./files/ILSVRC2012_val_00031649.JPEG"
+    img_path = './data/0_29.jpg'
+    # img_path =  "./data/ILSVRC2012_val_00031649.JPEG"
+    # img_path =  "./data/a.jpg"
     output_dir = current_dir+'/output';
     # model_path = 'D:\OneDrive\研究生\模式识别\大作业\pretrain_mae_vit_base_mask_0.75_400e.pth';
     model_path = 'D:\OneDrive\研究生\模式识别\大作业\cifar10-model\checkpoint-159.pth';
@@ -27,6 +28,6 @@ if __name__=='__main__':
     # checkpoint = torch.load(args.model_path, map_location=args.device)
     # mae_model.load_state_dict(checkpoint['model'])
     # mae_model.eval()
-    args.img_path = './data/a.jpg'
+    # args.img_path = './data/a.jpg'
 
     run_mae_vis.main(args)
